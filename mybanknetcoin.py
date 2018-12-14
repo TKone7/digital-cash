15538c94a7d8d010e2bb27ba862f0114158ba156
Tobias Koller <koller.tobias@gmx.net>
2018-12-14 10:12:38 +0800

    solved changable outputs by including it to the signing message

---
+++ mybanknetcoin.py
@@ -0,0 +1,110 @@
+import uuid
+from copy import deepcopy
+from ecdsa import SigningKey, SECP256k1
+from utils import serialize
+
+
+class Tx:
+
+    def __init__(self, id, tx_ins, tx_outs):
+        self.id = id
+        self.tx_ins = tx_ins
+        self.tx_outs = tx_outs
+
+    def sign_input(self, index, private_key):
+        message = self.spend_message(index)
+        signature = private_key.sign(message)
+        self.tx_ins[index].signature = signature
+
+    def verify_input(self, index, public_key):
+        tx_in = self.tx_ins[index]
+        message = self.spend_message(index)
+        public_key.verify(tx_in.signature, message)
+    
+    def spend_message(self, index):
+        tx_in = self.tx_ins[index]
+        outpoint = tx_in.outpoint
+        return serialize(outpoint) + serialize(self.tx_outs)
+
+class TxIn:
+
+    def __init__(self, tx_id, index, signature=None):
+        self.tx_id = tx_id
+        self.index = index
+        self.signature = signature
+
+    @property
+    def outpoint(self):
+        return (self.tx_id, self.index)
+
+class TxOut:
+
+    def __init__(self, tx_id, index, amount, public_key):
+        self.tx_id = tx_id
+        self.index = index
+        self.amount = amount
+        self.public_key = public_key
+
+    @property
+    def outpoint(self):
+        return (self.tx_id, self.index)
+
+class Bank:
+
+    def __init__(self):
+        # all transactions
+        #self.txs = {}
+        # (tx_id, index) -> tx_out
+        self.utxo = {}
+
+    def update_utxo(self, tx):
+        for tx_in in tx.tx_ins:
+            del self.utxo[tx_in.outpoint]
+        for tx_out in tx.tx_outs:
+            self.utxo[tx_out.outpoint] = tx_out
+
+    def issue(self, amount, public_key):
+        id_ = str(uuid.uuid4())
+        tx_ins = []
+        tx_outs = [TxOut(tx_id=id_, index=0, amount=amount, public_key=public_key)]
+        tx = Tx(id=id_, tx_ins=tx_ins, tx_outs=tx_outs)
+        #self.txs[tx.id] = tx
+        self.update_utxo(tx)
+        return tx
+
+    def validate_tx(self, tx):
+        in_sum = 0
+        out_sum = 0
+        for tx_in in tx.tx_ins:
+            #assert self.is_unspent(tx_in)
+            assert tx_in.outpoint in self.utxo
+
+            tx_out = self.utxo[tx_in.outpoint]
+            # Verify signature using public key of TxOut we're spending
+            public_key = tx_out.public_key
+            tx.verify_input(tx_in.index, public_key)
+
+            # Sum up the total inputs
+            amount = tx_out.amount
+            in_sum += amount
+
+        for tx_out in tx.tx_outs:
+            out_sum += tx_out.amount
+
+        assert in_sum == out_sum
+
+    def handle_tx(self, tx):
+        # Save to self.txs if it's valid
+        self.validate_tx(tx)
+        #self.txs[tx.id] = tx
+        self.update_utxo(tx)
+
+    def fetch_utxo(self, public_key):
+        return [utxo for utxo in self.utxo.values()
+                if utxo.public_key.to_string() == public_key.to_string()]
+
+    def fetch_balance(self, public_key):
+        # Fetch utxo associated with this public key
+        unspents = self.fetch_utxo(public_key)
+        # Sum the amounts
+        return sum([tx_out.amount for tx_out in unspents])
